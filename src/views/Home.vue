6527cbee6b2e4fe258bc4eb586226f80c0d4dfef
Maximiliano Orellana <maximiliano.orellana.a@usach.cl>
2019-11-08 12:58:34 -0300

    added reference page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>
-    <h1></h1>
-
-  </div>
+  <b-container>
+    <hr>
+    <b-row>
+    <b-col><b-img src="https://i.imgur.com/bcPr8uJ.png" width="256" fluid alt="Responsive image"></b-img></b-col>
+    <b-col cols="6">
+    <h1>I wanna cooperate.</h1>
+    <p>Wanacoop es una aplicación web diseñada para mantenerte segur@ durante una manifestación. Con ayuda de los usuarios, Wanacoop ofrece un mapa actualizado en tiempo real de todo lo que acontece alrededor de una manifestación.</p>
+    </b-col>
+    </b-row>
+    <hr>
+    <h1>Simbología</h1>
+    <hr>
+    <b-row>
+    <b-col cols="6">
+    <h1>Pánico</h1>
+    <p>Símbolo que representa el pedido de ayuda de un usuario de Wanacoop.</p>
+    </b-col>
+    <b-col><b-img src="https://i.imgur.com/BQWADZJ.png" width="128" fluid alt="Responsive image"></b-img></b-col>
+    </b-row>
+    <b-row>
+    <b-col cols="6">
+    <h1>Fuerzas de Órden Público</h1>
+    <p>Símbolo que representa la presencia de fuerzas de orden público en la zona.</p>
+    </b-col>
+    <b-col><b-img src="https://i.imgur.com/g5Cenpx.png" width="128" fluid alt="Responsive image"></b-img></b-col>
+    </b-row>
+    <b-row>
+    <b-col cols="6">
+    <h1>Patrulla</h1>
+    <p>Símbolo que representa la presencia de patrullaje en la zona.</p>
+    </b-col>
+    <b-col><b-img src="https://i.imgur.com/igrHsR1.png" width="128" fluid alt="Responsive image"></b-img></b-col>
+    </b-row>
+    <b-row>
+    <b-col cols="6">
+    <h1>Carro lanza agua</h1>
+    <p>Símbolo que representa la presencia de carro lanza agua en la zona.</p>
+    </b-col>
+    <b-col><b-img src="https://i.imgur.com/0ydOdUp.png" width="128" fluid alt="Responsive image"></b-img></b-col>
+    </b-row>
+    <b-row>
+    <b-col cols="6">
+    <h1>Bloqueo</h1>
+    <p>Símbolo que representa el bloqueo de una calle a causa de barricadas u otras razones.</p>
+    </b-col>
+    <b-col><b-img src="https://i.imgur.com/vfhgCUG.png" width="128" fluid alt="Responsive image"></b-img></b-col>
+    </b-row>
+    <b-row>
+    <b-col cols="6">
+    <h1>Ayuda</h1>
+    <p>Símbolo que representa la presencia de usuarios y lugares que prestan servicios de salud y primeros auxilios.</p>
+    </b-col>
+    <b-col><b-img src="https://i.imgur.com/0XObLbV.png" width="128" fluid alt="Responsive image"></b-img></b-col>
+    </b-row>
+  </b-container>
 </template>
